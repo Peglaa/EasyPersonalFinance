418fd68c8813493d1f3a686866650a874baec424
Pegla <stipa.damir@hotmail.com>
2021-07-29 18:54:12 +0200

    Completely reworking the app
    Added the V2 implementation of the app. Complete rework from scratch. Better layouts, better coding principles, better performance, using what I've learned so far to make it better.

---
+++ EasyPersonalFinanceV2/app/src/main/java/android/damir/stipancic/com/easypersonalfinancev2/UtilityClass.java
@@ -0,0 +1,30 @@
+package android.damir.stipancic.com.easypersonalfinancev2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.core.content.ContextCompat;
+
+public class UtilityClass {
+
+    Context mContext;
+
+    public UtilityClass(Context context){
+        this.mContext = context;
+    }
+
+    public static void setStatusBarColor(Activity activity) {
+        Window window = activity.getWindow();
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(activity, R.color.colorPrimaryDark));
+    }
+}
