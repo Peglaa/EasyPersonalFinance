8a97b3b4194e9ee1166a5ea2831a60249c5c93a9
Pegla <stipa.damir@hotmail.com>
2021-08-01 19:15:06 +0200

    Remove status bar function as well and add Xlint re-compile option
    Remove forgotten status bar function fron Utility class and its deprecated methods and also add Xlint compile option to help with deprecated warnings in the future

--- EasyPersonalFinanceV2(rework WIP)/app/src/main/java/android/damir/stipancic/com/easypersonalfinancev2/UtilityClass.java
+++ EasyPersonalFinanceV2(rework WIP)/app/src/main/java/android/damir/stipancic/com/easypersonalfinancev2/UtilityClass.java
@@ -17,19 +17,6 @@ public class UtilityClass {
         this.mContext = context;
     }
 
-    public static void setStatusBarColor(Activity activity) {
-        Window window = activity.getWindow();
-
-        // clear FLAG_TRANSLUCENT_STATUS flag:
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
-        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-
-        // finally change the color
-        window.setStatusBarColor(ContextCompat.getColor(activity, R.color.colorPrimaryDark));
-    }
-
     public static void setupToolbar(AppCompatActivity activity, int toolbar_resource){
         Toolbar toolbar = activity.findViewById(toolbar_resource);
         activity.setSupportActionBar(toolbar);
