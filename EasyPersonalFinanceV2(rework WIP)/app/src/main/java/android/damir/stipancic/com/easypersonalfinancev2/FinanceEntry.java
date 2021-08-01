8defa4ce264903c93df6ae1fbc8d46e58360e70d
Pegla <stipa.damir@hotmail.com>
2021-08-01 23:10:25 +0200

    More Realm preparation
    More Realm preparation

--- EasyPersonalFinanceV2(rework WIP)/app/src/main/java/android/damir/stipancic/com/easypersonalfinancev2/FinanceEntry.java
+++ EasyPersonalFinanceV2(rework WIP)/app/src/main/java/android/damir/stipancic/com/easypersonalfinancev2/FinanceEntry.java
@@ -3,10 +3,14 @@ package android.damir.stipancic.com.easypersonalfinancev2;
 import java.util.Date;
 import java.util.UUID;
 
-public class FinanceEntry {
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
 
-    private final UUID mId;
-    private final int mType;
+public class FinanceEntry extends RealmObject {
+
+    @PrimaryKey private final UUID mId;
+    @Required private final int mType;
     private final String mName;
     private Date mDate;
     private Date mDueDate;
@@ -24,5 +28,53 @@ public class FinanceEntry {
         this.mDate = date;
         this.mDueDate = dueDate;
         this.mAmount = amount;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public int getType() {
+        return mType;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public Date getDueDate() {
+        return mDueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        mDueDate = dueDate;
+    }
+
+    public float getAmount() {
+        return mAmount;
+    }
+
+    public void setAmount(float amount) {
+        mAmount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "FinanceEntry{" +
+                "mId=" + mId +
+                ", mType=" + mType +
+                ", mName='" + mName + '\'' +
+                ", mDate=" + mDate +
+                ", mDueDate=" + mDueDate +
+                ", mAmount=" + mAmount +
+                '}';
     }
 }
